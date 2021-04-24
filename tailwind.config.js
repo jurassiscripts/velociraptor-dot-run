aeda330e9fb89826a4d396b16a1f299c16a4f881
Umberto Pepato <umbo.pepato@gmail.com>
2021-04-24 17:11:42 +0200

    Whitelist dynamic twind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 module.exports = {
   darkMode: 'media',
-  purge: [
-    "index.njk",
-    "404.html",
-    "./_includes/**/*.{html,njk}",
-  ],
+  purge: {
+    options: {
+      safelist: ['ml-2', 'ml-4', 'ml-6'],
+    },
+    content: [
+      "index.njk",
+      "404.html",
+      "./_includes/**/*.{html,njk}",
+    ],
+  },
   variants: {
     extend: {
       textColor: ['group-focus'],
