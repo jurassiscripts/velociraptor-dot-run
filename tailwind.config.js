e0a0f72a774dc8c3d6300647e13d9d84cc8b92de
Umberto Pepato <umbo.pepato@gmail.com>
2022-06-20 15:58:21 +0200

    Migrate to Tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 module.exports = {
-  darkMode: 'media',
-  purge: {
-    options: {
-      safelist: ['ml-2', 'ml-4', 'ml-6'],
+  darkMode: 'class',
+  content: [
+    'index.njk',
+    '404.html',
+    './_includes/**/*.{html,njk}',
+  ],
+  safelist: ['ml-2', 'ml-4', 'ml-6'],
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          dark: '#09142a',
+        },
+      },
     },
-    content: [
-      "index.njk",
-      "404.html",
-      "./_includes/**/*.{html,njk}",
-    ],
   },
   variants: {
     extend: {
       textColor: ['group-focus'],
     },
   },
-}
+};
